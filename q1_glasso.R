66ff748cdc14c2ecfc1588b0892cdf41298addc6
qchenclaire <qchen42@jhu.edu>
2019-04-27 09:08:28 -0400

    question 1

--- q1_glasso.R
+++ q1_glasso.R
@@ -3,4 +3,5 @@ data1<-read.table("data.txt")
 cm = cov(data1, y = NULL, use = "all.obs", method = "pearson")
 g.fit = glasso(cm, 3)
 #number of edges
-print((sum(g.fit$w != 0)-20)/2)
+n = ncol(data1)
+print((sum(g.fit$w != 0)-n)/2)
